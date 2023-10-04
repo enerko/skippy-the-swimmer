7a8d5c1bde4d5f86d360d35724389037081c848a
enerko <rene.koike@mail.utoronto.ca>
2023-10-04 01:53:26 -0400

    Fixed tree and added sound effects

--- Assets/Scripts/KnockObject.cs
+++ Assets/Scripts/KnockObject.cs
@@ -6,16 +6,28 @@ using UnityEngine.Animations.Rigging;
 
 public class KnockObject : MonoBehaviour
 {
-    [SerializeField] Vector3 direction;
+    [SerializeField] private Vector3 direction;
     private Rigidbody _rb;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip breakSound;
+    [SerializeField] private AudioClip fallSound;
+    private bool isFalling = false;
 
 
     public IEnumerator Knock()
     {
+        
         _rb = gameObject.AddComponent<Rigidbody>();
         _rb.AddForce(direction, ForceMode.Impulse);
-        yield return new WaitForSeconds(5);
+        audioSource.clip = breakSound;
+        audioSource.Play();
+        isFalling = true;
+        yield return new WaitForSeconds(3);
+        audioSource.clip = fallSound;
+        audioSource.Play();
         Destroy(_rb);
+        
 
     }
+
 }
