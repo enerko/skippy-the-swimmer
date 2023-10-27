5b010fef6059c58fb0f40ff62b080e95f86fc99a
enerko <rene.koike@mail.utoronto.ca>
2023-10-26 21:29:42 -0400

    Added collectible sounds

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -1,21 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
     private Collectibles _collectibles;
+    private AudioClip collectibleSound;
 
-    public string description;
 
     void Start() {
         _collectibles = FindObjectOfType<Collectibles>();
+        collectibleSound = Resources.Load<AudioClip>("sfx_collectible");
     }
 
     void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            Vector3 pos = gameObject.transform.position;
+            AudioSource.PlayClipAtPoint(collectibleSound, pos);
             Destroy(gameObject);
             _collectibles.CollectNew();
         }
