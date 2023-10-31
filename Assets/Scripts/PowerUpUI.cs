221bc0949babbdec105d295022502ebe2f330e45
Justin Zheng <jz17392@gmail.com>
2023-10-31 17:53:40 -0400

    removed old powerup ui, now has a filter overlay for when skippy receives double jump buff, removed speed powerup

--- Assets/Scripts/PowerUpUI.cs
+++ Assets/Scripts/PowerUpUI.cs
@@ -5,16 +5,18 @@ using UnityEngine.UI;
 
 public class PowerUpUI : MonoBehaviour
 {
-    private Slider _slider;
-    private Image _icon;
-    
+    private Image _filterImage; // Reference to the Image component on this GameObject
     private float _time = 0;
 
     private void Start()
     {
-        _time = 0;
-        _icon = transform.GetChild(2).GetComponent<Image>();
-        _slider = transform.GetComponent<Slider>();
+        // Get the Image component from this GameObject
+        _filterImage = GetComponent<Image>();
+        if (_filterImage != null)
+        {
+            // Start with the image disabled
+            _filterImage.enabled = false;
+        }
     }
 
     void Update()
@@ -22,18 +24,22 @@ public class PowerUpUI : MonoBehaviour
         if (_time > 0)
         {
             _time -= Time.deltaTime;
-            float normalizedTime = Mathf.Clamp01(_time / PlayerPowerup.PowerUpDuration);
-
-            _slider.value = normalizedTime;
-        } else
+        }
+        else if (_filterImage != null && _filterImage.enabled)
         {
-            _icon.color = new Color32(175, 175, 175, 100);
+            // When the power-up duration ends, disable the image
+            _filterImage.enabled = false;
         }
     }
 
     public void Show()
     {
         _time = PlayerPowerup.PowerUpDuration;
-        _icon.color = new Color32(255, 255, 255, 255);
+
+        // Enable the image when the power-up is active
+        if (_filterImage != null)
+        {
+            _filterImage.enabled = true;
+        }
     }
 }
