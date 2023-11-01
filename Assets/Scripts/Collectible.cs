5125273bdb59165b8c88e138e8ac23723787ba68
hana-dowe <han_d87@outlook.com>
2023-11-01 12:00:11 -0400

    fix duplicate collectible collection

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -10,6 +10,8 @@ public class Collectible : MonoBehaviour
     private AudioClip collectibleSound;
     public event Action<GameObject> ObjectCollectedEvent;
 
+    private bool _collected = false;
+
     void Start() {
         _collectibles = FindObjectOfType<Collectibles>();
         collectibleSound = Resources.Load<AudioClip>("sfx_collectible");
@@ -17,8 +19,9 @@ public class Collectible : MonoBehaviour
 
     void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" && !_collected)
         {
+            _collected = true;
             ObjectCollectedEvent?.Invoke(gameObject);
             Vector3 pos = gameObject.transform.position;
             float volume = PlayerPrefs.GetFloat("SFX Volume", 1);
