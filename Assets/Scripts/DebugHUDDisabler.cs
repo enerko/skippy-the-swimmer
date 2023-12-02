4a504b74e7ee48a3454659932cc595f6ff4d9e56
Andy Wang <andy_wang02@hotmail.com>
2023-12-02 18:18:13 -0500

    debug mode UI disabler, fixed dad aduio

---
+++ Assets/Scripts/DebugHUDDisabler.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attach this to the main UI to enable/disable it during runtime in editor/debug builds
+public class DebugHUDDisabler : MonoBehaviour
+{
+    private Canvas _canvas;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _canvas = GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Debug.isDebugBuild && Input.GetKeyDown(KeyCode.H)) {
+            _canvas.enabled = !_canvas.enabled;
+        }
+    }
+}
