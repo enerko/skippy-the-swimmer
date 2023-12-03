18aff9340048d03f7671e99e0cda9bf74b58019a
Andy Wang <andy_wang02@hotmail.com>
2023-12-03 18:00:11 -0500

    trying to make fan move player

---
+++ Assets/Scripts/PlatformStand.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformStand : MonoBehaviour
+{
+    private GameObject _target;
+    private Vector3 _offset;
+
+
+    void Update() {
+        if (!_target) {
+            return;
+        }
+
+        // manually apply offset and rotation
+        _target.transform.position = transform.position + transform.TransformDirection(_offset);
+    }
+
+    // Assume collider can only collide with the player
+    void OnCollisionStay(Collision other)
+    {
+        _target = other.collider.gameObject;
+        _offset = transform.InverseTransformDirection(_target.transform.position - transform.position);
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        _target = null;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,7 +19,7 @@ using System;
      public static Vector3 s_HorizInput;
  
      [SerializeField]
-     private float _baseSpeed = 7;
+     private float _baseSpeed = 8;
      private float _drySpeed = 4;
      private const float Jump = 15f;  // initial velocity
      private bool hasUsedDoubleJump = false;
