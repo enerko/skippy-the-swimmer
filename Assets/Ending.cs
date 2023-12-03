2ffc5d800dd60cfb449168ddffd12cdbc215168c
enerko <rene.koike@mail.utoronto.ca>
2023-12-02 21:53:42 -0500

    Added new camera angle for cutscene

--- Assets/Ending.cs
+++ Assets/Ending.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
 
 public class Ending : MonoBehaviour
 {
+    private PlayableDirector director;
+    private GameObject gameUI;
+
     // Start is called before the first frame update
     void Start()
     {
+        director = GetComponent<PlayableDirector>();
+        gameUI = GameObject.Find("Game UI");
         
     }
 
@@ -14,5 +21,12 @@ public class Ending : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        director.Play();
+        gameUI.SetActive(false);
+        CameraMain.s_CutSceneActive = true;
     }
 }
