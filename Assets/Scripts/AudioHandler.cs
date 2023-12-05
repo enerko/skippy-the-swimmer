1ca5f8ac438fdc6efaabd1a862fb9518d40cb18d
Justin Zheng <jz17392@gmail.com>
2023-12-04 22:33:13 -0500

    fixed tutorial music to play correctly instead of level 1 music

--- Assets/Scripts/AudioHandler.cs
+++ Assets/Scripts/AudioHandler.cs
@@ -2,25 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class AudioHandler : MonoBehaviour
 {
     public AudioSource audioSource;
     public AudioClip wakeupMusic;
+    public AudioClip tutorialMusic; // New AudioClip for the tutorial level
     public AudioClip level1Music;
     public Player player;
 
     public void Start()
     {
         PlayableDirector director = player.GetComponent<PlayableDirector>();
+
         if (director != null && !Globals.s_Restarted)
         {
             StartCoroutine(PlayWakeupMusic());
         }
         else
         {
-            audioSource.clip = level1Music;
-            audioSource.Play();
+            // Check if the current level is the tutorial
+            if (IsTutorialLevel())
+            {
+                // Play the tutorial music
+                audioSource.clip = tutorialMusic;
+                audioSource.Play();
+            }
+            else
+            {
+                // Play level 1 music
+                audioSource.clip = level1Music;
+                audioSource.Play();
+            }
         }
     }
 
@@ -31,5 +45,11 @@ public class AudioHandler : MonoBehaviour
         audioSource.clip = level1Music;
         audioSource.Play();
     }
-}
 
+    // Method to determine if the current level is the tutorial
+    private bool IsTutorialLevel()
+    {
+        return SceneManager.GetActiveScene().name == "Tutorial";
+    }
+
+}
